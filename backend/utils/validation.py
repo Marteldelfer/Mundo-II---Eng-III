6d04e8562ea2809f9a550e5b5b0314318dc13f7a
Marteldelfer <marteldelfer@gmail.com>
2026-09-06 20:56:37 -0300

    módulo de validação

---
+++ backend/utils/validation.py
@@ -0,0 +1,33 @@
+# Módulo para validação de dados
+import re
+
+
+def validEmail(email: str) -> list[str]:
+    """Retorna lista de mensagens de error"""
+    if not re.match(r'/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', email):
+        return ['Email inválido']
+
+
+def validPassword(password: str) -> list[str]:
+    """Retorna lista de mensagens de error"""
+    errors = []
+    if not len(password) >= 8:
+        errors.append('A senha deve ter pelo menos 8 caracteres')
+    if not any(c.isupper() for c in password):
+        errors.append('A senha deve ter pelo menos uma letra maiúscula')
+    if not any(c.isdigit() for c in password):
+        errors.append('A senha deve ter pelo menos um número')
+    if not any(not c.isalnum() for c in password):
+        errors.append('A senha deve ter pelo menos um caractere especial')
+
+    return errors
+
+
+def validName(name: str) -> list[str]:
+    """Retorna lista de mensagens de error"""
+    errors = []
+    if not any(c.isdigit() for c in name) or not any(not c.isalnum() for c in name):
+        errors.append('O nome não pode conter caracteres especiais ou números')
+    if len(name) <= 0:
+        errors.append('O nome é obrigatório')
+    return errors
