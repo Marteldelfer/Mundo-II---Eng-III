1b0826e2951d7b9fd9289d28631a17f4b8e50315
gabmar-gf <gabrielmgf1112@gmail.com>
2026-09-07 19:33:33 -0300

    conexao dos botoes com paginas de login e cadastro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import logo from './assets/logo.png'
+import Cadastro from './pages/cadastro'
+import Login from './pages/login'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
@@ -8,35 +11,45 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <section id="center">
-        <div className="logo">
-          <img src={logo} className="base" width="200" height="200" alt="" />
-        </div>
-        <div>
-          <h1>Bem vindo ao AdaptEd.</h1>
-          <h2>A inclusão na sua sala de aula começa com um clique.</h2>
-        </div>
-        <div style={{display: 'flex', gap: '20px'}}>
-        <button
-          style={{width: '300px', height: '75px'}}
-          type="button"
-          className="signin-button"
-          onClick={() => alert('Realizar cadastro')}
-          >
-          Criar conta
-        </button>
-        <button
-          style={{width: '300px', height: '75px'}}
-          type="button"
-          className="login-button"
-          onClick={() => alert('Realizar login')}
-        >
-          Entrar
-        </button>
-        </div>
-      </section>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <section id="center">
+              <div className="logo">
+                <img src={logo} className="base" width="200" height="200" alt="" />
+              </div>
+              <div>
+                <h1>Bem vindo ao AdaptEd.</h1>
+                <h2>A inclusão na sua sala de aula começa com um clique.</h2>
+              </div>
+              <div style={{display: 'flex', gap: '20px'}}>
+              <Link to="/cadastro">
+                <button
+                  style={{width: '300px', height: '75px'}}
+                  type="button"
+                  className="signin-button"
+                  >
+                  Criar conta
+                </button>
+              </Link>
+              <Link to="/login">
+                <button
+                  style={{width: '300px', height: '75px'}}
+                  type="button"
+                  className="login-button"
+                >
+                  Entrar
+                </button>
+              </Link>
+              </div>
+            </section>
+          </>
+        } />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
